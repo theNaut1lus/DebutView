590d9717b422cf12a86f77731376460bd048c021
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-19 19:01:31 +1100

    Initial Commit

---
+++ DebutView/DebutViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebutViewApp.swift
+//  DebutView
+//
+//  Created by Sidak Singh Aulakh on 19/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct DebutViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
