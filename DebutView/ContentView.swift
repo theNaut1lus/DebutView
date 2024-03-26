c04539358af314724607e40dd705a3938e3e026d
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-26 17:33:41 +1100

    class 19/03

--- DebutView/ContentView.swift
+++ DebutView/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var toggle = false
+    @State var text = "said no"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(.debut)
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "swift")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.regularMaterial)
+                Text("Should've")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.regularMaterial)
+                Button(action: {
+                    toggle.toggle()
+                    text = (toggle) ? "said no" : "gone home"
+                }, label: {
+                    Text(text)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundStyle(.debut)
+                        .padding()
+                        .background(.regularMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 50.0))
+                })
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .tint(.gray)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
